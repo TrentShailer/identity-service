74dc8cc9ec23b3810ec2cb132c4f9bcb44913021
Trent Shailer <trent.shailer@gmail.com>
2025-07-28 17:28:36 +1200

    feat: change ts-api-helper to crates.io release

--- frontend/scripts/config.ts
+++ frontend/scripts/config.ts
@@ -3,6 +3,7 @@ import { Header, setConfig as setFetchConfig } from "../lib/fetch.ts";
 export const API_URL = "http://localhost:8081";
 export const API_KEY: Header = ["X-TS-API-Key", "identity-site"];
 // TODO could API Key be moved to fetch config
+// TODO handle dev config vs prod config?
 
 export function setConfig() {
   setFetchConfig("");
